5e4a7f26858e91045688bd782a66cc8ab8a985fb
Oliwer <oe222ez@student.lnu.se>
2022-06-18 18:00:01 +0200

    Add dashboard link on start page

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Start.css'
 
 const Start = () => {
   return (
-    <div>Start</div>
+    <div id='start'>
+      <div>
+        <Link className='link' to="/dashboard">Open Dashboard &#x2192;</Link>
+      </div>
+    </div>
   )
 }
 
