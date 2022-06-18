5e9c096a87aa24de6d22fadad36292d1fc0ca112
Oliwer <oe222ez@student.lnu.se>
2022-06-18 18:10:45 +0200

    Add dashboard layout

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import './Dashboard.css'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div id='dashboard'>
+      <h1>Dashboard</h1>
+      <div id='chart-container'>
+        {/* Add charts/gauges here! */}
+      </div>
+    </div>
   )
 }
 
