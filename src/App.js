9e7f9671a7af830cde9d05505532a15a21744201
Oliwer <oe222ez@student.lnu.se>
2022-06-18 17:45:10 +0200

    Add start, dashboard and page not found page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Start from './components/Start';
+import Dashboard from './components/Dashboard';
+import PageNotFound from './components/PageNotFound';
 
 function App() {
   return (
   <>
     <Header />
     <main>
-      
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Start />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Router>
     </main>
     <Footer />
   </>
