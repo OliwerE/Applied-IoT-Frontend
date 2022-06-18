5d5bd97219f9d56bf4e9cda7d631256ad140ba82
Oliwer <oe222ez@student.lnu.se>
2022-06-18 18:34:04 +0200

    Render sensor data in the dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Dashboard.css'
 
 const Dashboard = () => {
+  const [sensors, setSensors] = useState([])
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_BASE_URL}/sensors`).then(res => {
+      return res.json()
+    }).then(json => {
+      console.log(json)
+      setSensors(json.sensors)
+    }).catch(err => {
+      console.error(err)
+    })
+  },[])
+
   return (
     <div id='dashboard'>
       <h1>Dashboard</h1>
       <div id='chart-container'>
-        {/* Add charts/gauges here! */}
+        {sensors.map(s => {
+          return <p>{s.sensorName}: {s.value}</p>
+        })}
       </div>
     </div>
   )
