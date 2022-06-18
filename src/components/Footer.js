3bf5da76303fdae31f77f6696e3cd3def3dd18cc
Oliwer <oe222ez@student.lnu.se>
2022-06-18 17:36:59 +0200

    Add header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      <p>IoT dashboard created by Oliwer Ellréus</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div id="header">
+      <h1>IoT Dashboard</h1>
+      <nav>
+        <ul>
+          <li>Start</li>
+          <li>Dashboard</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
