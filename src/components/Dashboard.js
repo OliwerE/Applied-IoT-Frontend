2625bbcd837865a3f7c5eaefb03df1d947146be9
Oliwer <oe222ez@student.lnu.se>
2022-06-20 15:22:49 +0200

    Refactor Gauges

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,30 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import GaugeChart from 'react-gauge-chart'
 
 import './Dashboard.css'
-import Gauge from './Gauge'
+import Gauges from './Gauges'
+
 
 const Dashboard = () => {
-  const [sensors, setSensors] = useState([])
-
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_BASE_URL}/sensors`).then(res => {
-      return res.json()
-    }).then(json => {
-      console.log(json)
-      setSensors(json.sensors)
-    }).catch(err => {
-      console.error(err)
-    })
-  },[])
 
   return (
     <div id='dashboard'>
       <h1>Dashboard</h1>
       <div id='chart-container'>
-        {sensors.map(sensor => {
-          return <Gauge sensor={sensor} />
-        })}
+        <Gauges />
       </div>
     </div>
   )
---
+++ src/components/Gauges.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import Gauge from './Gauge'
+
+const Gauges = () => {
+  const [sensors, setSensors] = useState([])
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_BASE_URL}/sensors`).then(res => {
+      return res.json()
+    }).then(json => {
+      console.log(json)
+      setSensors(json.sensors)
+    }).catch(err => {
+      console.error(err)
+    })
+  },[])
+
+  return (
+    <>
+      {sensors.map(sensor => {
+        return <Gauge sensor={sensor} />
+      })}
+  </>
+  )
+}
+
+export default Gauges
