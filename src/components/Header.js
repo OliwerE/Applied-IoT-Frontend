ba50402384b041347f27aff265ab23c664a8c7a7
Oliwer <oe222ez@student.lnu.se>
2022-06-26 14:44:18 +0200

    Add responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,10 @@ import './Header.css'
 const Header = () => {
   return (
     <div id="header">
-      <img width={50} src='/logo192.png' alt='logo'/>
-      <h1>IoT Dashboard</h1>
+      <div>
+        <img width={50} src='/logo192.png' alt='logo'/>
+        <h1>IoT Dashboard</h1>
+      </div>
       <nav>
         <ul>
           <li><Link className='link' to="/">Start</Link></li>
